090784525e42c2b2fe6a5cda378c9a09e0dc31ae
Alison Gabriel <odevalison@gmail.com>
2025-11-13 21:41:48 -0300

    feat: ensure password and password confirmation coincide on signup form

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -29,31 +29,36 @@ import {
 import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, {
-    message: 'O nome é obrigatório.',
-  }),
-  lastName: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório',
-  }),
-  email: z
-    .string()
-    .email({
-      message: 'O e-mail digitado é inválido.',
-    })
-    .min(1, {
-      message: 'O e-mail é obrigatório.',
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, {
+      message: 'O nome é obrigatório.',
     }),
-  password: z.string().trim().min(6, {
-    message: 'A senha deve ter no mínimo 6 caracteres.',
-  }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: 'A confirmação da senha é obrigatória.',
-  }),
-  terms: z.boolean().refine((fieldValue) => fieldValue === true, {
-    message: 'Você precisa aceitar os termos.',
-  }),
-})
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório',
+    }),
+    email: z
+      .string()
+      .email({
+        message: 'O e-mail digitado é inválido.',
+      })
+      .min(1, {
+        message: 'O e-mail é obrigatório.',
+      }),
+    password: z.string().trim().min(6, {
+      message: 'A senha deve ter no mínimo 6 caracteres.',
+    }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'A confirmação da senha é obrigatória.',
+    }),
+    terms: z.boolean().refine((fieldValue) => fieldValue === true, {
+      message: 'Você precisa aceitar os termos.',
+    }),
+  })
+  .refine((fields) => fields.password === fields.passwordConfirmation, {
+    message: 'As senhas não coincidem.',
+    path: ['passwordConfirmation'],
+  })
 
 type SignupSchema = z.infer<typeof signupSchema>
 
